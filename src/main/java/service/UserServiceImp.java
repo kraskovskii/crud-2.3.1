436fd1744541e8158501f1113fcb183def3a3ce8
Kraskovskii <kraskovskii.m@yandex.ru>
2021-07-31 01:25:14 +0300

    init

--- src/main/java/service/UserServiceImp.java
+++ src/main/java/service/UserServiceImp.java
@@ -6,14 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImp implements UserService{
+public class UserServiceImp {//implements UserService{
 
-    @Autowired
-    private UserDao userDao;
-
-    @Override
-    public void add(User user) {
-        userDao.add(user);
-    }
+//    @Autowired
+//    private UserDao userDao;
+//
+//    @Override
+//    public void add(User user) {
+//        userDao.add(user);
+//    }
 
 }
